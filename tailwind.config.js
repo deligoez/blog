555f9c82d747cecb18b0b7eb0b493b0573abbe17
Yunus Emre Deligöz <ye@deligoz.me>
2021-03-31 20:51:26 +0300

    Update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 module.exports = {
-    purge: {
-      content: [
+    purge: [
         './resources/**/*.antlers.html',
         './resources/**/*.blade.php',
         './content/**/*.md'
-      ]
-    },
+    ],
+    darkMode: false, // or 'media' or 'class'
     important: true,
     theme: {
-      extend: {},
+        extend: {
+            fontFamily: {
+                'serif': ['Equity Text A', 'Times New Roman', 'Times', 'Georgia', 'Cambria', 'ui-serif', 'serif'],
+                'caps': ['Equity Caps A', 'serif'],
+                'index': ['concourse-t3-index', 'sans'],
+            },
+        },
     },
-    variants: {},
+    variants: {
+        extend: {},
+    },
     plugins: [],
-  }
+}
