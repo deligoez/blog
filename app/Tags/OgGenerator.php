7579e5aeff6a411136a6921feab6df9eb84980a0
Yunus Emre Deligöz <ye@deligoz.me>
2025-12-17 12:42:43 +0300

    Add og:image dimensions and type meta tags for better validator support

--- app/Tags/OgGenerator.php
+++ app/Tags/OgGenerator.php
@@ -38,7 +38,11 @@ class OgGenerator extends Tags
 
         $this->makeImageIfNotExists($title, $filename);
 
-        return "<meta property='og:image' content='{$url}' />\n<meta name='twitter:image' content='{$url}' />";
+        return "<meta property='og:image' content='{$url}' />\n" .
+               "<meta property='og:image:width' content='1920' />\n" .
+               "<meta property='og:image:height' content='1080' />\n" .
+               "<meta property='og:image:type' content='image/png' />\n" .
+               "<meta name='twitter:image' content='{$url}' />";
     }
 
     private function makeImageIfNotExists(string $title, string $filename)
